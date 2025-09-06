39dff3bdda4696aeb9b3bd9225be1ad10c5fe239
l-lacavalla <l.lacavalla@studenti.poliba.it>
2025-09-06 18:16:00 +0200

    Implementata la funzione di aggiunta del follow ad un portale o ad un utente (in followController)

--- controller/followController.js
+++ controller/followController.js
@@ -5,10 +5,7 @@ const {HttpError} = require("../middleware/errorMiddleware");
 
 //lista utenti che segui
 //lista portali che segui
-//visualizzazione in base alle 4 flag
-//edit campi delle flag
 //add follow
-
 
 //passa da verifyToken
 async function getFollowedUsers(req,res,next){
@@ -93,5 +90,46 @@ async function getFollowedPortals(req,res,next){
     }
 }
 
-module.exports = {getFollowedUsers, getFollowedPortals}
+// valida sia per i portali che per gli utenti
+async function addFollowed(req, res, next){
+    const userId = req.user.id
+    const addId = req.params.id;
+    try{
+        const user = await BasicUser.findById(userId)
 
+        let follow = await BasicUser.findById(addId)
+        if(!follow){
+            follow = await Portal.findById(addId)
+            if(!follow){
+                throw new HttpError("Portal/User not found",404)
+            }
+
+            //controllo che il portale non venga già seguito dall'utente che fa la richiesta
+            if(user.followedPortals.includes(follow._id)){
+                throw new HttpError(`You already follow the portal: ${follow.name}`,409)
+            }
+
+            //aggiungo l'id del portale in followedPortals del BasicUser
+            console.log(user.followedPortals)
+            user.followedPortals.push(follow._id)
+            await user.save()
+            console.log(user.followedPortals)
+            return res.status(200).json({ok:true, message:"Portal followed successfully"})
+        }
+
+        //controllo che l'utente non venga già seguito dall'utente che fa la richiesta
+        if(user.followedResearchers.includes(follow._id)){
+            throw new HttpError(`You already follow the user: ${follow.realName}`,409)
+        }
+        console.log(user.followedResearchers)
+        //aggiungo l'id dell'utente in followedPortals del BasicUser
+        user.followedResearchers.push(follow._id)
+        await user.save()
+        console.log(user.followedResearchers)
+        return res.status(200).json({ok:true, message:"Researcher followed successfully"})
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {getFollowedUsers, getFollowedPortals, addFollowed}
--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
-const {getFollowedUsers, getFollowedPortals} = require('../controller/followController')
+const {getFollowedUsers, getFollowedPortals, addFollowed} = require('../controller/followController')
 const {verifyToken} = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
 router.get('/users', verifyToken, getFollowedUsers)
 router.get('/portals', verifyToken, getFollowedPortals)
+router.post('/add/:id', verifyToken, addFollowed)
 
 module.exports = router
